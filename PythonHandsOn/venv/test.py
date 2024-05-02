1fc4fdef914db95b6f95a7055a424db5842f3ddd
Bhavik Vaghela <vaghelabhavib@gmail.com>
2024-05-02 21:48:11 +0530

    Running Commit

--- PythonHandsOn/venv/test.py
+++ PythonHandsOn/venv/test.py
@@ -1,16 +1,8 @@
-
 class Solution(object):
-    def isValid(self, s):
-        l = len(s)
-        d = {}
-        for i in range(0,l,1):
-            if (s[i] != " "):
-                if d.get(s[i])==None:
-                    d[s[i]] = 1
-                else:
-                    d[s[i]] = d.get(s[i]) + 1
-        return d
-
+    def temp(self, nums):
+       
+        return len(result)
 
 if __name__ == '__main__':
-    print(Solution().isValid("ABHISHEK BAHADUR SINGH"))
+    t = [1,1,2,3,4,5,5,6]
+    print(Solution().temp(t))
---
+++ PythonHandsOn/venv/test2.py
@@ -0,0 +1,46 @@
+# print(5/2)
+# print(5//2)
+# print(5%2)
+# print(5**2)
+
+# i = 2.45
+# j = 4
+# print(i*j)
+
+# s = "5.65"
+# print(float(s[0:1])%5)
+# s1 = "Hi" ' how are you'
+# print(s1)
+
+# s = "Hi \nhow are you"
+# print(s)
+
+# s="Hi "
+# s1 = "How are you"
+# print(s, end='')
+# print(s1)
+
+# i=5//2
+# s="5**2"
+# print (s[-3:-1])
+# print(s[0])
+# print(len(s))
+# print(float(i))
+# i = 512
+# s =str(i)
+# print()
+
+l = ["Hi", 5//2, 5%2, 5/2, 15.6]
+# print(l[0:3])
+# for i in range(0,len(l), 1):
+#     print(l[i], end=', ')
+# l1 = l
+# l2 = list(l)
+# l[1] = "hello"
+# print(l)
+# print(l1)
+# print(l2)
+
+
+
+
